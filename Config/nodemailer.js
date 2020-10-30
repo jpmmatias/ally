b374a857c7cd730cd9e5fdb781675f2e1797800e
João Pedro Matias <jpmmatias2018@outlook.com>
2020-10-30 09:10:13 -0300

    nodemailer

---
+++ Config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer')
+const xoauth2 = require('xoauth2')
+const keys = require('./keys');
+
+
+let transporter = nodemailer.createTransport({
+  
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: 'allyticc@gmail.com',
+        clientId: keys.GOOGLE_CLIENT_ID,
+        clientSecret:keys.GOOGLE_CLIENT_SECRET,
+        refreshToken: keys.REFRESH_TOKEN,
+        accessToken: keys.ACESS_TOKEN,
+        },
+    
+})
+
+let testerAcietou =(nomeTester,email,teste)=> {
+    return(
+    {
+        from: 'Ally <allyticc@gmail.com>',
+        to: email,
+        subject: 'Convite para teste aceito!',
+        html: `<h1>Ally</h1>
+                <h2>Seu convite para o teste ${teste} foi aceito ${nomeTester} </h2>`}
+    )
+}
+
+module.exports={
+    testerAcietou,
+    transporter
+
+}
