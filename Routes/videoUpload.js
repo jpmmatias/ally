e97e71dfad062379dc2405e2860bdc87e441781e
João Pedro Matias <jpmmatias2018@outlook.com>
2020-11-05 15:38:14 -0300

    update

--- Routes/videoUpload.js
+++ Routes/videoUpload.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const fs = require('fs');
 const router = express.Router({mergeParams: true});
 const {ensureAuthenticated} = require('../Config/auth');
 const multer = require('multer');
 const {videoUpload} = require('../Controllers/videoUpload')
-const path = require('path');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
-const keys = require('../Config/keys');
 const Video = require('../Models/Video');
 
 //Servidor local
@@ -28,28 +23,6 @@ const Video = require('../Models/Video');
  })
 const upload = multer({storage})
 
-
-//AWS S3
-//  const s3 = new aws.S3({apiVersion:'2006-03-1'})
-//  s3.config.update({
-//     secretAccessKey:keys.S3_KEY_SECRET ,
-//     accessKeyId: keys.S3_KEY_ID,
-//     region: 'sa-east-1'
-// });
-
-//  const upload = multer(
-//      {storage:multerS3({
-//          acl: 'public-read',
-//          s3:s3,
-//          bucket:'allyvideos',
-//          metadata:(req,file,cb)=>{
-//              cb(null,{fieldName:file.fieldname})
-//          },
-//          key:(req,file,cb)=>{
-//              cb(null,file.originalname)
-//          }
-//      })}
-//      )
 
 router.post('/',ensureAuthenticated,upload.single('video'),videoUpload)
 module.exports = router;
