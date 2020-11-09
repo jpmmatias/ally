7eb272c25e37e27a9194de65148694e791730b4c
João Pedro Matias <jpmmatias2018@outlook.com>
2020-11-09 11:47:59 -0300

    update

--- Controllers/chamadas.js
+++ Controllers/chamadas.js
@@ -3,9 +3,6 @@ const io = require('../Sockets/Sockets');
 //Modelo Teste
 const Teste = require('../Models/Teste');
 
-//Modelo User
-const User = require('../Models/User');
-
 //Modelo Anotação
 const Anotacao = require('../Models/Anotacao');
 
@@ -22,30 +19,6 @@ exports.mostrarSala = async (req, res, next) => {
 			res.render('Chamadas/sala', {
 				teste,
 				id,
-			});
-		}
-	} catch (err) {
-		console.log(err);
-		return res.render('Erros/404');
-	}
-};
-
-//descrição         Mostrar lobby de um teste
-//route             GET /
-//Acesso            Privado
-exports.mostrarLobby = async (req, res, next) => {
-	let id = req.params.id;
-	try {
-		let user = await User.findById(req.session.passport.user);
-		let teste = await Teste.findById(req.params.id).populate('user').lean();
-		if (!teste) {
-			return res.render('Erros/404');
-		} else {
-			io.emit('entrou no lobby', teste, id, user);
-			res.render('Chamadas/lobby', {
-				teste,
-				id,
-				user,
 			});
 		}
 	} catch (err) {
