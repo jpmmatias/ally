6e06bee7529e4f7c7ed7bb58cdd2b3a517d77f43
João Pedro Matias <jpmmatias2018@outlook.com>
2020-11-16 20:20:43 -0300

    Update convite testers

--- Public/Javascript/convite.js
+++ Public/Javascript/convite.js
@@ -5,66 +5,63 @@ const pegarID = () => {
 	return ROOM_ID;
 };
 
-let mandarConvite =(nome)=>{
-    let id = pegarID()
-    fetch(`http://localhost:5000/testes/add/${id}/convidar/`, {  
-        method: 'POST', 
-        headers:{
-            'Content-Type':'application/json'
-        }, 
-        body: JSON.stringify({
-        nome: nome,
-      })
-    })
-    .then( (data)=> {  
-      console.log('Request success: ', data);  
-    })  
-    .catch( (err)=> {  
-      console.log('Request failure: ', err);  
-    });
-}
+let mandarConvite = (nome) => {
+	let id = pegarID();
+	fetch(`http://localhost:5000/testes/add/${id}/convidar/`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			nome: nome
+		})
+	})
+		.then((data) => {
+			console.log('Request success: ', data);
+		})
+		.catch((err) => {
+			console.log('Request failure: ', err);
+		});
+};
 
+let aceitarConvite = (userNome, userId, testeNome, testeId) => {
+	fetch(`http://localhost:5000/testes/add/${testeId}/convidar/aceitar`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			userNome: userNome,
+			userId: userId,
+			testeNome: testeNome,
+			testeId: testeId
+		})
+	})
+		.then((data) => {
+			console.log('Request success: ', data);
+		})
+		.catch((err) => {
+			console.log('Request failure: ', err);
+		});
+};
 
-let aceitarConvite =(userNome,userId,testeNome,testeId)=>{
-    fetch(`http://localhost:5000/testes/add/${testeId}/convidar/aceitar`, {  
-        method: 'POST', 
-        headers:{
-             'Content-Type':'application/json'
-         }, 
-         body: JSON.stringify({
-            userNome: userNome,
-            userId:userId,
-            testeNome:testeNome,
-            testeId:testeId   
-       })
-       
-     })
-     .then( (data)=> {  
-       console.log('Request success: ', data);  
-     })  
-     .catch( (err)=> {  
-       console.log('Request failure: ', err);  
-     });
-}
-
-let recusarConvite =(userNome,userId,testeNome,testeId)=>{
-  fetch(`http://localhost:5000/testes/add/${testeId}/convidar/recusar`, {  
-      method: 'POST', 
-      headers:{
-           'Content-Type':'application/json'
-       }, 
-       body: JSON.stringify({
-          userNome: userNome,
-          userId:userId,
-          testeNome:testeNome,
-          testeId:testeId   
-     })
-     
-   })
-   .then( (data)=> {  
-     console.log('Request success: ', data);  
-   })  
-   .catch( (err)=> {  
-     console.log('Request failure: ', err);  
-   });
-}
+let recusarConvite = (userNome, userId, testeNome, testeId) => {
+	fetch(`http://localhost:5000/testes/add/${testeId}/convidar/recusar`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			userNome: userNome,
+			userId: userId,
+			testeNome: testeNome,
+			testeId: testeId
+		})
+	})
+		.then((data) => {
+			console.log('Request success: ', data);
+		})
+		.catch((err) => {
+			console.log('Request failure: ', err);
+		});
+};
